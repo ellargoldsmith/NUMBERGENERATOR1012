c64f32034d539e19d1c049826689aded44a5b050
Goldsmith <1ergoldsmith@seattleschools.org>
2023-10-03 09:28:52 -0700

    This is my practice at submitting things to git and github

---
+++ ShortSentence.java
@@ -0,0 +1,15 @@
+// Ella Goldsmith, Ap CS, Oct 3, 2023
+// This program prints out a short sentence specific to me
+
+public class ShortSentence {
+
+   public static void main(String[] args) {
+      specificSentence();
+   }
+   // My short sentence about me.
+   public static void specificSentence() {
+   
+      System.out.println("My is Ella and my zodiac sign is a cancer.");
+   }
+}
+
