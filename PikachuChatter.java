7803c15a712600bd2beb1d57087c92eddda0de4c
Goldsmith <1ergoldsmith@seattleschools.org>
2023-10-03 09:02:43 -0700

    Pleasde ignore

---
+++ PikachuChatter.java
@@ -0,0 +1,36 @@
+// Ella Goldsmith, Ap CS, Sept 28, 2023
+// This program displays Pikachu Chatter.
+
+public class PikachuChatter {
+
+   public static void main(String[] args) {
+      wordsLineOne();
+      pikachuChatter();
+      wordsLineTwo();
+      pikachuChatter();
+   }
+   // First line of words.
+   public static void wordsLineOne() {
+   
+      System.out.println("Pika pika pika chu pika chu peeeee ka pika chu!"); 
+   }
+   // Pikachu Chatter symbols.
+   public static void pikachuChatter() {
+   
+      System.out.println("(\\__/)");
+      System.out.println("(o^.^)");
+      System.out.println("z(_(\")(\")");
+      System.out.println("");
+  }
+  // Second Line of words.
+  public static void wordsLineTwo() {
+  
+      System.out.println("Pika? Pika pika pika chu peeeee ka chu!");
+  }
+}
+  
+
+      
+   
+      
+      
