d91e9cb9a5b599e0007934cdb4665a6a7f54a33d
Goldsmith <1ergoldsmith@seattleschools.org>
2023-10-09 08:59:17 -0700

    Thsi is program was an assignment where I created my own object class, and gave it placeholder methods.

---
+++ Pencil.java
@@ -0,0 +1,26 @@
+// Ella Goldsmith, Ap CS, Oct 6, 2023
+// In this program I will be creating my own Object Class, and giving it placeholer methods.
+public class Pencil {
+   public static void main(String[] args) {
+   }
+      //Sharpen pencil.
+      public static void sharpenPencil()
+      {
+      }
+      //Write words with pencil.
+      public static void writeWords()
+      {
+      }
+      //Earase with pencil.
+      public static void eraseWithPencil()
+      {
+      }
+      //Draw a picture with pencil.
+      public static void drawPicture()
+      {
+      }
+      //Lend pencil to a friend.
+      public static void lendToAFriend()
+      {
+      }
+}
---
+++ ProgressCheck.java
@@ -0,0 +1,12 @@
+public class ProgressCheck {
+   public static void main(String[] args) {
+   
+     System.out.print("cat ");
+
+      System.out.println("dog ");
+      
+      System.out.println("horse ");
+      
+      System.out.print("cow ");   
+   }
+}
